f459adde8de1a019048dc23bc301f8b208a73591
MC^2 <98896096+OriginalNam3@users.noreply.github.com>
2022-02-23 13:38:48 +0000

    Update S1.py

--- CCC/2022/Senior/S1.py
+++ CCC/2022/Senior/S1.py
@@ -3,7 +3,7 @@ n = int(input())
 fours = n // 4
 remainder = n % 4
 
-if fours >= remainder: # Each unit in remainder can be distributed to a four in the sum to make it a five
-    print((fours - remainder)//5 + 1) # Check how many groups of five fours there are in the fours (as they can be made into groups of four fives)
-else:
-    print(0)
+# Each unit in remainder can be distributed to a four in the sum to make it a five
+# So we just have to check how many groups of five fours there are in the fours (as they can be made into groups of four fives)
+
+print((fours - remainder)//5 + 1 if fours >= remainder else 0) 
