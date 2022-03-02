196f116a5dac7f64da6a21a60ec98de5a3dffb93
MC^2 <98896096+OriginalNam3@users.noreply.github.com>
2022-03-02 22:00:44 +0000

    Update D.EasterEggs.py

--- Wednesdays/2-3-2022/D.EasterEggs.py
+++ Wednesdays/2-3-2022/D.EasterEggs.py
@@ -1,12 +1,14 @@
+# This problem 
+
 n = int(input())
 
 colours = ['R', 'O', 'Y', 'G', 'B', 'I', 'V']
 
-print('BIVROYG', end='') # print all colours so the condition is fulfilled.
+print('BIVROYG', end='') # print all colours so the condition is fulfilled
 n -= 7
-for i in range(n // 4):
-    for j in range(4):
-        print(colours[j], end='')
-for i in range(n%4):
-    print(colours[i], end='')
-# Just keep cycliing through four colours as the three colours at the beginning will not be the same as 
+# Now we have to make sure that four sequential eggs are the same colour
+# This can simply be done by looping through the four colours at the end of the first seven colours printed
+for i in range(n):
+    print(colours[i % 4], end='')
+# Note that because the first three colours are never used within the cycle
+# Therefore, no matter what colour you end on, the first three colours at the beginning and the colour at the end will not overlap
